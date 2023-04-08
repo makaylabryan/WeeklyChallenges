0f9d786cf1d45cbffa5a38141c80b5b97c732ab7
Makayla Bryan <makaylabryan@gmail.com>
2023-04-07 20:19:54 -0400

    Weekly Challenges set 2 completed

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +9,121 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+           return char.IsLetter(c);
         }
-
+        public bool CharacterIsNotALetter(char x)
+        {
+            return char.IsDigit(x);
+        }
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            if (vals.Length %2 == 0) 
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 == 0) 
+            {
+                return true;
+            }
+            return false;
         }
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+            return false;
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            if (numbers.Count() == 0) 
+            {
+                return 0;
+            }
+            return numbers.Min() + numbers.Max();
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            if (str1.Length > str2.Length)
+            {
+                return str2.Length;
+            }
+            return str1.Length;
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            { 
+                return 0; 
+            }
+            if (numbers.Length == 0)
+            { 
+                return 0; 
+            }
+            return numbers.Sum();
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            { 
+                return 0; 
+            }
+            var evenSum = 0;
+
+            for (int i = 0; i < numbers.Length; i++) 
+            {
+                if (numbers[i] % 2 == 0) 
+                {
+                    evenSum += numbers[i];
+                }
+            }
+            return evenSum;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            { 
+                return false; 
+            }
+            if (numbers.Capacity == 0)
+            { 
+                return false; 
+            }
+            if (numbers.Sum() % 2 != 0)
+            {
+                return true;
+            }
+            return false;
         }
 
         public long CountOfPositiveOddsBelowNumber(long number)
         {
-            throw new NotImplementedException();
+            var nums = new List<long>();
+
+            for (long i = 0; i < number; i++)
+            {
+                if (i % 2 != 0 && i > 0)
+                {
+                    nums.Add(i);
+                }
+            }
+            return nums.Count;
         }
     }
 }
