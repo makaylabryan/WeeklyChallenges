2f93a8840aea53947d8c38c6eb3cf5c258e5557e
Makayla Bryan <makaylabryan@gmail.com>
2023-04-27 16:22:51 -0400

    weeklyChallenges 4

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +9,107 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    sum += number;
+                }
+                else if (number % 2 == 1)
+                {
+                    sum -= number;
+                }
+                else return 0;
+            }
+            return sum;
+
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            var numList = new List<int>() { str1.Length, str2.Length, str3.Length, str4.Length };
+            return numList.Min();
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            var numbers = new List<int>() { number1, number2, number3, number4 };
+            return numbers.Min();
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+           return (
+                sideLength1 + sideLength2 > sideLength3 &&
+                sideLength1 + sideLength3 > sideLength2 &&
+                sideLength2 + sideLength3 > sideLength1
+                );
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            var isNumber = double.TryParse(input, out double number) ;
+           
+            return isNumber;
+            
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            var elementsNum = objs.Length;
+            var majority = (elementsNum / 2) + 1;
+
+            var count = 0;
+            foreach ( var obj in objs ) 
+            {
+                if (obj == null)
+                {
+                    count++;
+                }
+            }
+            if (count >= majority)
+            {
+                return true;
+            }
+            return false;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            { return 0; }
+
+            var evenNums = numbers.Where(nums => nums % 2 == 0);
+
+            if (numbers.Count() == 0)
+            { return 0; }
+
+            if (evenNums.Count() == 0)
+            { return 0; }
+
+                       
+            return evenNums.Average();
+            
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            var factorial = 1;
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            for (int i = number; i > 0; i--)
+            {
+                factorial *= i;
+            }
+            return factorial;
         }
     }
 }
